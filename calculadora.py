66b36157413a21ef252b731b1a3a8bbbcfc46b16
mangelgarcia1973 <mangel.garcia@gmail.com>
2025-04-09 12:15:25 +0200

    aceptamos el cambio

--- calculadora.py
+++ calculadora.py
@@ -6,7 +6,7 @@ def calculadora():
     seguir = True
     while seguir:
         print("Calculadora simple en Python")
-        print("Operaciones disponibles: +, -, *, /")
+        print("Operaciones disponibles y ahora cambiamos tambien aqui: +, -, *, /")
 
         a = float(input("Introduce el primer número para sumar: "))
         operacion = input("Introduce la operación (+, -, *, /): o S para salir")
